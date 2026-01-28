b7c2259d1d573d373011b93b852231fe8f422a9c
Padmalatha5 <163010157+Padmalatha5@users.noreply.github.com>
2026-01-28 10:26:23 +0530

    Add files via upload

---
+++ sample.java
@@ -0,0 +1,11 @@
+class sample{
+    public static void main(String args[])
+    {
+        //My first java sample code
+        System.out.println("Hello ,this is a Sample Program");
+        /*happy to learning the journey of java
+         * easy to execute
+         * difficulties for understanding
+         */
+    }
+}
